bd5e84afba889658e7dfa1fba707e6ff43b2ed54
Alex Swenson <swensona@charter.net>
2016-02-13 17:13:20 -0600

    Added a basic play button and a skeleton method for when the button is pressed to start the quiz.

--- app/src/main/java/com/example/alexswenson/basicquizhw1/MainActivity.java
+++ app/src/main/java/com/example/alexswenson/basicquizhw1/MainActivity.java
@@ -3,11 +3,35 @@ package com.example.alexswenson.basicquizhw1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStartQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        btnStartQuiz = (Button)findViewById(R.id.btnStartQuiz);
+        btnStartQuiz.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                startQuiz(view);
+            }
+        });
+    }
+
+    public void startQuiz(View view) {
+
+
+
     }
 }
